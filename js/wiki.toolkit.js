5ad98a7fa7e35c827f5d0f27b4ef9d2a828e93bc
jonkee <zzzgoo@gmail.com>
2019-04-21 20:52:32 +0800

    bugs

--- js/wiki.toolkit.js
+++ js/wiki.toolkit.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         wikipedia toolkit
-// @version      0.9
+// @version      1.0.0
 // @updateURL    https://openuserjs.org/meta/zzzope/My_Script.meta.js
 // @description  nothing
 // @author       zzzope
@@ -53,10 +53,12 @@
 		}
 	})
 	// redirect
-	let chLink = document.querySelector(".interlanguage-link.interwiki-zh a")
 
-	if (chLink && chLink.href) {
-		chLink.href = chLink.href.replace(/org\/.*\//, "org/zh-tw/")
+	if (window.location.hostname.includes(".wikipedia.org")) {
+		let chLink = document.querySelector(".interlanguage-link.interwiki-zh a")
+		if (chLink && chLink.href) {
+			chLink.href = chLink.href.replace(/org\/.*\//, "org/zh-tw/")
+		}
 	}
 	if (window.location.hostname === "zh.wikipedia.org") {
 		if (!url.includes("zh-tw")) {
